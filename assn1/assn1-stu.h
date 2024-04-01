3f86557e16b8f2ad3065498dfd0af3efb4217c34
gitchannn <eunkeeeee@gmail.com>
2024-04-01 11:10:58 +0000

    feat: initial commit

---
+++ assn1/assn1-stu.h
@@ -0,0 +1,37 @@
+#ifndef __ASSN1_STU_H__
+#define __ASSN1_STU_H__
+
+#include "assn1.h"
+
+template<size_t N>
+void NANDGate<N>::advanceCycle() {
+  /* FIXME */
+}
+
+template<size_t N>
+void SignedAdder<N>::advanceCycle() {
+  /* FIXME */
+}
+
+template<size_t N>
+void SignedSubtractor<N>::advanceCycle() {
+  /* FIXME */
+}
+
+template<size_t N>
+void LogicalUnit<N>::advanceCycle() {
+  /* FIXME */
+}
+
+template<size_t N, size_t M>
+void HashTable<N, M>::advanceCycle() {
+  /* FIXME */
+}
+
+template<size_t N, size_t D>
+void DelayQueue<N, D>::advanceCycle() {
+  /* FIXME */
+}
+
+#endif
+
