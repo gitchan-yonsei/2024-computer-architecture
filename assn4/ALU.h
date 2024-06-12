cc1fe94b167292a2bd181eb50e6f58962bd9f59d
gitchan <eunkeeeee@gmail.com>
2024-06-12 22:24:35 +0900

    feat: ALU

--- assn4/ALU.h
+++ assn4/ALU.h
@@ -22,7 +22,49 @@ class ALU : public DigitalCircuit {
     }
 
     virtual void advanceCycle() {
-      /* FIXME */
+        // INPUT
+        // input0: ALU의 첫 번째 32비트 입력값
+        // input1: ALU의 두 번째 32비트 입력값
+
+        // OUTPUT
+        // output: ALU의 32비트 출력값
+        // zero: ALU의 zero flag -> output이 0이면 1로 설정됨
+
+        // 파라미터를 받아오고 초기값 설정
+        uint32_t aluControl = static_cast<uint32_t>(_iALUControl->to_ulong());
+        uint32_t input0 = static_cast<uint32_t>(_iInput0->to_ulong());
+        uint32_t input1 = static_cast<uint32_t>(_iInput1->to_ulong());
+        uint32_t output = 0;
+
+        // ALU Control에 따라 연산의 종류 결정
+      switch (aluControl) {
+        case 0b0000: // AND
+          output = input0 & input1;
+          break;
+        case 0b0001: // OR
+          output = input0 | input1;
+          break;
+        case 0b0010: // add
+          output = input0 + input1;
+          break;
+        case 0b0110: // subtract
+          output = input0 - input1;
+          break;
+        case 0b0111: // set on less than
+          output = (input0 < input1) ? 1 : 0;
+          break;
+        case 0b1100: // NOR
+          output = ~(input0 | input1);
+          break;
+        default:
+          assert(false && "지원하지 않는 ALU Control입니다.");
+      }
+
+      // Set the output value
+      *_oOutput = std::bitset<32>(output);
+
+      // Set the zero flag
+      *_oZero = std::bitset<1>(output == 0);
     }
 
   private:
