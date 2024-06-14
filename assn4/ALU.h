9671e64de10874dc06a13f8c705ec8e4f40cc165
깃짱 <107979804+gitchannn@users.noreply.github.com>
2024-06-14 21:53:53 +0900

    feat: assn3 내용 반영 (#34)
    * feat: ALU
    
    * feat: ALU Control
    
    * feat: Control
    
    * feat: Memory
    
    * feat: Register File

--- assn4/ALU.h
+++ assn4/ALU.h
@@ -3,37 +3,142 @@
 
 #include "DigitalCircuit.h"
 
+#include <iostream>
 #include <cassert>
 
 class ALU : public DigitalCircuit {
 
-  public:
-
+public:
     ALU(const Wire<4> *iALUControl,
         const Wire<32> *iInput0,
         const Wire<32> *iInput1,
         Wire<32> *oOutput,
         Wire<1> *oZero) : DigitalCircuit("ALU") {
-      _iALUControl = iALUControl;
-      _iInput0 = iInput0;
-      _iInput1 = iInput1;
-      _oOutput = oOutput;
-      _oZero = oZero;
+        _iALUControl = iALUControl;
+        _iInput0 = iInput0;
+        _iInput1 = iInput1;
+        _oOutput = oOutput;
+        _oZero = oZero;
     }
 
     virtual void advanceCycle() {
-      /* FIXME */
+        _oOutput->reset();
+        _oZero->reset();
+
+        // ALU that supports 32bit signed integer AND, OR, ADD, SUB, SLT, NOR
+        // 0000: AND, 0001: OR, 0010: ADD, 0110: SUB, 0111: SLT, 1100: NOR
+
+        // print 0, 1, 2, 3 bits of alucontrol in order
+        // std::cout << "ALU Control: " << _iALUControl->test(3) << _iALUControl->test(2) << _iALUControl->test(1) << _iALUControl->test(0) << std::endl;
+
+        // 0000 AND
+        if (_iALUControl->test(3) == false &&
+            _iALUControl->test(2) == false &&
+            _iALUControl->test(1) == false &&
+            _iALUControl->test(0) == false) {
+            // std::cout << "AND" << std::endl;
+            for (size_t i = 0; i < 32; i++) {
+                _oOutput->set(i, _iInput0->test(i) && _iInput1->test(i));
+            }
+        }
+            // 0001 OR
+        else if (_iALUControl->test(3) == false &&
+                 _iALUControl->test(2) == false &&
+                 _iALUControl->test(1) == false &&
+                 _iALUControl->test(0) == true) {
+            // std::cout << "OR" << std::endl;
+            for (size_t i = 0; i < 32; i++) {
+                _oOutput->set(i, _iInput0->test(i) || _iInput1->test(i));
+            }
+        }
+            // 0010 ADD
+        else if (_iALUControl->test(3) == false &&
+                 _iALUControl->test(2) == false &&
+                 _iALUControl->test(1) == true &&
+                 _iALUControl->test(0) == false) {
+            unsigned tmp = 0;
+            for (size_t i = 0; i < 32; i++) {
+                if (_iInput0->test(i)) {
+                    tmp++;
+                }
+                if (_iInput1->test(i)) {
+                    tmp++;
+                }
+                _oOutput->set(i, (tmp % 2) == 1 ? true : false);
+                tmp /= 2;
+            }
+            if (tmp > 0) {
+                _oZero->set(0, true);
+            }
+            // print add input0 + input1 = output  in one line
+            // std::cout << "Add Input0: " << _iInput0->to_ulong() << " + Input1: " << _iInput1->to_ulong() << " = Output: " << _oOutput->to_ulong() << std::endl;
+        }
+            // 0110 SUB
+        else if (_iALUControl->test(3) == false &&
+                 _iALUControl->test(2) == true &&
+                 _iALUControl->test(1) == true &&
+                 _iALUControl->test(0) == false) {
+            unsigned carry = 1;
+            for (size_t i = 0; i < 32; i++) {
+                if (_iInput0->test(i)) {
+                    carry++;
+                }
+                if (!(_iInput1->test(i))) {
+                    carry++;
+                }
+                _oOutput->set(i, (carry % 2) == 1 ? true : false);
+                carry /= 2;
+            }
+            if (carry > 0) {
+                _oZero->set(0, true);
+            }
+            // print sub input0 - input1 = output  in one line
+            // std::cout << "Sub Input0: " << _iInput0->to_ulong() << " - Input1: " << _iInput1->to_ulong() << " = Output: " << _oOutput->to_ulong() << std::endl;
+        }
+            // 0111 SLT
+            // set 1 if iInput0 is less than iInput1, otherwise set 0
+        else if (_iALUControl->test(3) == false &&
+                 _iALUControl->test(2) == true &&
+                 _iALUControl->test(1) == true &&
+                 _iALUControl->test(0) == true) {
+            bool isLess = false;
+            for (size_t i = 31; i >= 0; i--) {
+                if (_iInput0->test(i) && !(_iInput1->test(i))) {
+                    isLess = true;
+                    break;
+                } else if (!(_iInput0->test(i)) && _iInput1->test(i)) {
+                    isLess = false;
+                    break;
+                }
+            }
+            _oOutput->set(0, !isLess);
+            // print slt input0 < input1 = output  in one line
+            // std::cout << "SLT Input0: " << _iInput0->to_ulong() << " < Input1: " << _iInput1->to_ulong() << " = Output: " << _oOutput->to_ulong() << std::endl;
+        }
+            // 1100 NOR
+        else if (_iALUControl->test(3) == true &&
+                 _iALUControl->test(2) == true &&
+                 _iALUControl->test(1) == false &&
+                 _iALUControl->test(0) == false) {
+            // std::cout << "NOR" << std::endl;
+            for (size_t i = 0; i < 32; i++) {
+                _oOutput->set(i, !(_iInput0->test(i) || _iInput1->test(i)));
+            }
+        } else {
+            assert(false && "Invalid ALU control signal");
+        }
+        // set the zero signal to 1 if the output value is zero
+        if (_oOutput->to_ulong() == 0) {
+            _oZero->set(0, true);
+        }
     }
 
-  private:
-
+private:
     const Wire<4> *_iALUControl;
     const Wire<32> *_iInput0;
     const Wire<32> *_iInput1;
     Wire<32> *_oOutput;
     Wire<1> *_oZero;
-
 };
 
 #endif
-
--- assn4/RegisterFile.h
+++ assn4/RegisterFile.h
@@ -8,8 +8,7 @@
 
 class RegisterFile : public DigitalCircuit {
 
-  public:
-
+public:
     RegisterFile(const Wire<5> *iReadRegister1,
                  const Wire<5> *iReadRegister2,
                  const Wire<5> *iWriteRegister,
@@ -18,50 +17,96 @@ class RegisterFile : public DigitalCircuit {
                  Wire<32> *oReadData1,
                  Wire<32> *oReadData2,
                  const char *initFileName = nullptr)
-      : DigitalCircuit("RegisterFile") {
-      _iReadRegister1 = iReadRegister1;
-      _iReadRegister2 = iReadRegister2;
-      _iWriteRegister = iWriteRegister;
-      _iWriteData = iWriteData;
-      _iRegWrite = iRegWrite;
-      _oReadData1 = oReadData1;
-      _oReadData2 = oReadData2;
+            : DigitalCircuit("RegisterFile") {
+        _iReadRegister1 = iReadRegister1;
+        _iReadRegister2 = iReadRegister2;
+        _iWriteRegister = iWriteRegister;
+        _iWriteData = iWriteData;
+        _iRegWrite = iRegWrite;
+        _oReadData1 = oReadData1;
+        _oReadData2 = oReadData2;
 
-      for (size_t i = 0; i < 32; i++) {
-        _registers[i].reset();
-      }
-
-      if (initFileName != nullptr) {
-        // Each line of the memory initialization file consists of:
-        //   1) the target register index in decimal value
-        //   2) the eight-digit hexadecimal value of the data
-        // For example, "10 ABCD1234" stores 0xABCD1234 to register #10.
-        FILE *initFile = fopen(initFileName, "r");
-        assert(initFile != NULL);
-        std::uint32_t reg, val;
-        while (fscanf(initFile, " %u %x", &reg, &val) == 2) {
-          if (reg != 0) {
-            printf("INFO: $%02u <-- 0x%08lx\n", reg, (unsigned long)val);
-            _registers[reg] = val;
-          }
+        for (size_t i = 0; i < 32; i++) {
+            _registers[i].reset();
         }
-      }
+
+        if (initFileName != nullptr) {
+            // Each line of the memory initialization file consists of:
+            //   1) the target register index in decimal value
+            //   2) the eight-digit hexadecimal value of the data
+            // For example, "10 ABCD1234" stores 0xABCD1234 to register #10.
+            FILE *initFile = fopen(initFileName, "r");
+            assert(initFile != NULL);
+            std::uint32_t reg, val;
+            while (fscanf(initFile, " %u %x", &reg, &val) == 2) {
+                if (reg != 0) {
+                    printf("INFO: $%02u <-- 0x%08lx\n", reg, (unsigned long) val);
+                    _registers[reg] = val;
+                }
+            }
+        }
     }
 
     void printRegisters() {
-      for (size_t i = 0; i < 32; i++) {
-        if (_registers[i].any()) {
-          printf("  $%02lu = 0x%08lx\n", i, _registers[i].to_ulong());
+        for (size_t i = 0; i < 32; i++) {
+            if (_registers[i].any()) {
+                printf("  $%02lu = 0x%08lx\n", i, _registers[i].to_ulong());
+            }
         }
-      }
     }
 
     virtual void advanceCycle() {
-      /* FIXME */
+        uint32_t readReg1 = 0;
+        uint32_t readReg2 = 0;
+        uint32_t writeReg = 0;
+        uint32_t writeData = 0;
+        bool regWrite = _iRegWrite->test(0);
+
+        // For reading, RegWrite is 0.
+        // iReadRegister1 = $rs
+        // iReadRegister2 = $rt
+        // $rs and $rt are returned to oReadData1 and oReadData2, respectively.
+        _oReadData1->reset();
+        _oReadData2->reset();
+
+        for (size_t i = 0; i < 5; ++i) {
+            if (_iReadRegister1->test(i)) {
+                readReg1 |= (1 << i);
+            }
+            if (_iReadRegister2->test(i)) {
+                readReg2 |= (1 << i);
+            }
+            if (_iWriteRegister->test(i)) {
+                writeReg |= (1 << i);
+            }
+        }
+
+        uint32_t readData1 = _registers[readReg1].to_ulong();
+        uint32_t readData2 = _registers[readReg2].to_ulong();
+
+        for (size_t i = 0; i < 32; ++i) {
+            _oReadData1->set(i, (readData1 & (1 << i)) != 0);
+            _oReadData2->set(i, (readData2 & (1 << i)) != 0);
+        }
+        // printf("oReadData1 = 0x%08lx\n", (unsigned long)readData1);
+        // printf("oReadData2 = 0x%08lx\n", (unsigned long)readData2);
+
+        // For writing to a destination register, RegWrite is 1.
+        // Index of the destination register is iWriteRegister.
+        // Value to be written is iWriteData.
+        // No return value exists for writes.
+        // Writing to zero register is ignored.
+        if (regWrite && writeReg != 0) {
+            for (size_t i = 0; i < 32; ++i) {
+                if (_iWriteData->test(i)) {
+                    writeData |= (1 << i);
+                }
+            }
+            _registers[writeReg] = writeData;
+        }
     }
 
-  private:
-
+private:
     const Wire<5> *_iReadRegister1;
     const Wire<5> *_iReadRegister2;
     const Wire<5> *_iWriteRegister;
@@ -71,8 +116,6 @@ class RegisterFile : public DigitalCircuit {
     Wire<32> *_oReadData2;
 
     std::bitset<32> _registers[32];
-
 };
 
 #endif
-
